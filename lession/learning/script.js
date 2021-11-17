84d458ecb76508855d1f75fcde382fab462e6d9b
Mohammad Wasim <wasim.html@gmail.com>
2021-11-17 22:23:15 +0600

    updates 11-17-2021

---
+++ lession/learning/script.js
@@ -0,0 +1,16 @@
+
+////////////// DOM manipulation    Learning for document.getElementByID
+///////////// Get String value inside html tag using id
+
+//// Variable call and assign
+var text = 'Hello World';
+var a = 5;
+var b = 6;
+var c = a + b;
+
+document.getElementById('getvlaue').innerHTML = text;
+//window.alert(text);
+console.log(text);
+
+
+document.write(c);
---
+++ lession/practice/script.js
@@ -0,0 +1,4 @@
+document.getElementById('getvalue').innerHTML = 'Hello world';
+document.write('Hello World');
+window.alert('Hello World');
+console.log('hello world');
